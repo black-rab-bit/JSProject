abe5f20292aa086e4533a2dbe5cf41b989cea916
black-rab-bit <koroleva_tm@bk.ru>
2021-01-15 18:07:42 +0900

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+console.log('ОСНОВЫ')
+console.log('JAVASCRIPT')
+let message = "Kak dela?";
+console.log(message);
+let go = 'Right';
+if(3 > 2 ){
+    console.log(go);
+}
+
+if (1 === "1") {
+    console.log("Right");
+}   else {
+    console.log("Wrong");
+}
+let message1 = (92 > '11' && 58 < 100) ? 'Истина' : 'Ложь';
+console.log(message1);
+if(0) {
+    console.log('No');
+}   else if(" ") {
+    console.log('Yes');
+}
+
+let num = 0;
+    while (num < 5){
+        console.log(num);
+        num++;
+    }
+    let num1 = 8;
+    while(num1) {
+        console.log(num1);
+        num1--;
+    }
+myName = "Vova"
+console.log(myName);
+    var myName;
+
+console.log(!false && 11 || 18 && !'');
